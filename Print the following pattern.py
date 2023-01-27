d92f986e83e77802f4d4a590e424b072166226db
Hossein Rostami <80810796+ihosseinrostami@users.noreply.github.com>
2023-01-28 03:12:02 +0330

    Update Print the following pattern.py

--- Print the following pattern.py
+++ Print the following pattern.py
@@ -5,8 +5,10 @@
 #4 4 4 4 
 #5 5 5 5 5
 
+#get the range of number from user and save it in "len"
 len = int(input("enter your range of number:"))
 
+#create a loop to cout
 for num in range(len+1):
     for i in range(num):
         print(num , end =" ")
