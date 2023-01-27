944505f78ee47862cf96573069d974af818caf7f
Hossein Rostami <80810796+ihosseinrostami@users.noreply.github.com>
2023-01-28 03:08:35 +0330

    Add files via upload

---
+++ Print the following pattern.py
@@ -0,0 +1,13 @@
+#print 
+#1 
+#2 2 
+#3 3 3 
+#4 4 4 4 
+#5 5 5 5 5
+
+len = int(input("enter your range of number:"))
+
+for num in range(len+1):
+    for i in range(num):
+        print(num , end =" ")
+    print("\n")
